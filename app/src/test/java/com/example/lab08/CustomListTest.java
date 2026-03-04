a0d98d343fc7f72b9b559d9a7f24cb3d5af5671f
Yani <Yanixiv.dev@gmail.com>
2026-03-03 17:48:00 -0700

    added failing test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+@test
+	public void failTest(){
+		var foo = false;
+		assertTrue(foo);
+	}
 }
